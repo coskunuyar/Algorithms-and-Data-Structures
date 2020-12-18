3cc3fdd6efede4dbc41d592bd2d8b37d9589b21d
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-18 23:42:17 +0300

    latest

--- data-structures/3.Stack/index.js
+++ data-structures/3.Stack/index.js
@@ -1,47 +1,60 @@
-function Node(value) {
-  this.value = value;
-  this.next = null;
-  this.prev = null;
-}
-
-function Stack() {
-  this.head = null;
-  this.tail = null;
-  this.length = 0;
-}
-
-Stack.prototype.push = function (value) {
-  const newNode = new Node(value);
-  if (this.length === 0) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    this.tail.next = newNode;
-    newNode.prev = this.tail;
-    this.tail = newNode;
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.prev = null;
   }
-  this.length++;
-};
 
-Stack.prototype.pop = function () {
-  if (this.length === 0) return;
-  let poppedNode = this.tail;
-  if (this.length === 1) {
+  clearConnections(){
+    delete this.next;
+    delete this.prev;
+    return this;
+  }
+}
+
+class Stack{
+  constructor(){
     this.head = null;
     this.tail = null;
-  } else {
-    this.tail = this.tail.prev;
-    this.tail.next = null;
+    this.length = 0;
   }
-  this.length--;
-  return poppedNode;
-};
 
-// const stack = new Stack();
-// stack.push(1);
-// stack.push(2);
-// stack.push(3);
+  push(value){
+    const newNode = new Node(value);
+    if(this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    return ++this.length;
+  }
 
-// for (let i = 0; i < 3; i++) {
-//   console.log(stack.pop().value);
-// }
+  pop(){
+    if(this.length <= 0) return;
+    const poppedNode = this.tail;
+    if(this.length === 1){
+      this.head = null;
+      this.tail = null;
+    }else{
+      const newTail = this.tail.prev;
+      newTail.next = null;
+      this.tail = newTail;
+    }
+    this.length--;
+    return poppedNode.clearConnections();
+  }
+}
+
+const stack = new Stack();
+stack.push(0);
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+for(let i=0; i<5; i++){
+  console.log(stack.pop());
+}
