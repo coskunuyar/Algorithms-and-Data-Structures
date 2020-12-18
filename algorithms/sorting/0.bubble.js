85fad7227ecd34c37ac43b5eeabd94235fbaeaf1
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-18 22:25:23 +0300

    finalised

--- algorithms/sorting/0.bubble.js
+++ algorithms/sorting/0.bubble.js
@@ -1,18 +1,14 @@
 const bubbleSort = (arr) => {
-  let isAlreadySorted;
-
-  for(let i=arr.length; i>0; i--){
-    isAlreadySorted = true;
-    for(let j=0; j<i-1; j++){
+  for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length; j++){
       if(arr[j] > arr[j+1]){
         const temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
-
-        isAlreadySorted = false;
       }
     }
-    if(isAlreadySorted) break;
   }
   return arr;
 }
+
+console.log(bubbleSort([5,4,32,1]));
