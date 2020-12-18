c6d23c54101ba2721e5a2bd5ac87853f2748dbac
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-18 22:16:10 +0300

    Update 5.selection.js

--- algorithms/sorting/5.selection.js
+++ algorithms/sorting/5.selection.js
@@ -1,16 +1,18 @@
-const selectionSort = arr => {
+const selectionSort = (arr) => {
   for(let i=0; i<arr.length; i++){
     let lowest = i;
-    for(let j = i + 1; j<arr.length; j++){
+    for(let j=i; j<arr.length; j++){
       if(arr[j] < arr[lowest]){
         lowest = j;
       }
     }
     if(lowest !== i){
-      let temp = arr[i];
+      const temp = arr[i];
       arr[i] = arr[lowest];
       arr[lowest] = temp;
     }
   }
   return arr;
 }
+
+console.log(selectionSort([5,4,3,2,1,99]));
