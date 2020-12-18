644ae450aed8ab660d85cdd4259e4729d9849444
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 00:21:01 +0300

    finalised

--- data-structures/7.HashTable/index.js
+++ data-structures/7.HashTable/index.js
@@ -1,64 +1,61 @@
-const { randomBytes } = require('crypto');
+class HashTable{
+  constructor(size = 40){
+    this.storage = new Array(size);
+    this.salt = Math.random().toString().replace('.','');
+  }
 
-function HashTable(size = 40) {
-  this.storage = new Array(size);
-  this.salt = randomBytes(12).toString('hex');
+  _hash(key){
+    key += this.salt;
+    const prime_number = 31;
+    let hash = 0;
+    key.split('').forEach(char => {
+      hash = (hash*prime_number + char.charCodeAt(0) - 96) % this.storage.length;
+    })
+    return Math.abs(hash);
+  }
+
+  set(key , value){
+    const index = this._hash(key);
+    if(!this.storage[index]) this.storage[index] = [];
+    this.storage[index].push({ key , value });
+  }
+
+  get(key){
+    const index = this._hash(key);
+    let foundItem;
+    this.storage[index].forEach(item => {
+      if(item.key === key) foundItem = item;
+    })
+    return foundItem;
+  }
+
+  keys(){
+    const keys = [];
+    this.storage.forEach(items => {
+      items.forEach(item => {
+        keys.push(item.key);
+      })
+    })
+    return keys;
+  }
+
+  values(){
+    const values = [];
+    this.storage.forEach(items => {
+      items.forEach(item => {
+        values.push(item.value);
+      })
+    })
+    return values;
+  }
 }
 
-HashTable.prototype._hash = function (value) {
-  let hash = 0;
-  const primeNumber = 47;
-  const salted = value + this.salt;
-  for (let char of salted) {
-    hash = (hash * primeNumber + char.charCodeAt(0) - 96) % this.storage.length;
-  }
-  return Math.abs(hash);
-};
+const table = new HashTable();
+table.set('name','coskun');
+table.set('surname','uyar');
 
-HashTable.prototype.set = function (key, value) {
-  const index = this._hash(key);
-  if (!this.storage[index]) this.storage[index] = [];
-  this.storage[index].push([key, value]);
-};
+console.log(table.get('name'));
+console.log(table.get('surname'));
 
-HashTable.prototype.get = function (key) {
-  const index = this._hash(key);
-  if (!this.storage[index]) return;
-  let foundValue;
-  this.storage[index].forEach((item) => {
-    if (item[0] === key) {
-      foundValue = item[1];
-    }
-  });
-  return foundValue;
-};
-
-HashTable.prototype.keys = function () {
-  const keys = [];
-  this.storage.forEach((element) => {
-    if (element) {
-      element.forEach((item) => {
-        keys.push(item[0]);
-      });
-    }
-  });
-  return keys;
-};
-
-HashTable.prototype.values = function () {
-  const values = [];
-  this.storage.forEach((element) => {
-    if (element) {
-      element.forEach((item) => {
-        values.push(item[1]);
-      });
-    }
-  });
-  return values;
-};
-
-// const table = new HashTable();
-// table.set('name', 'Coskun');
-// table.set('surname', 'UYAR');
-// console.log(table.keys());
-// console.log(table.values());
+console.log(table.keys());
+console.log(table.values());
