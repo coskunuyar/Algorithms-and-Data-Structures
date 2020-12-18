4c835df55ed38cdd4553fa3c1260b5ac2544c08a
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-18 22:33:19 +0300

    Update 2.merge.js

--- algorithms/sorting/2.merge.js
+++ algorithms/sorting/2.merge.js
@@ -1,36 +1,36 @@
-const merge = (arr1, arr2 ) => {
-  let results = [];
-  let i=0;
-  let j=0;
+const mergeSort = (arr) => {
+  if(arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0 , mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left , right);
+}
 
-  while(i<arr1.length && j<arr2.length){
-    if(arr2[j] > arr[i]){
-      results.push(arr1[i]);
-      i++;
-    } else {
-      results.push(arr2[j]);
+const merge = (arr1 , arr2) => {
+  const result = [];
+  let i=0 , j=0;
+
+  while(i < arr1.length && j < arr2.length){
+    if(arr1[i] < arr2[j]){
+      result.push(arr1[i]);
+      i++
+    }else{
+      result.push(arr2[j]);
       j++;
     }
   }
 
-  while( i < arr1.length){
-    results.push(arr1[i]);
+  while(i < arr1.length){
+    result.push(arr1[i]);
     i++;
   }
 
-  while( j < arr2.length){
-    results.push(arr2[j]);
+  while(j < arr2.length){
+    result.push(arr2[j]);
     j++;
   }
 
-  return results;
+  return result;
 }
 
-const mergeSort = (arr) => {
-  if(arr.length <= 1) return arr;
-  let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0 , mid));
-  let right = mergeSort(arr.slice(mid));
-
-  return merge(left ,right);
-}
+console.log(mergeSort([7,5,5,4,3,2,1]));
