8089aba902bab6ff258c2c3ba25cc5cdd8e701a2
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 00:09:42 +0300

    finalised

--- data-structures/1.DoublyLinkedList/index.js
+++ data-structures/1.DoublyLinkedList/index.js
@@ -1,132 +1,142 @@
-function Node(value) {
-  this.value = value;
-  this.next = null;
-  this.prev = null;
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+
+  clearConnections(){
+    delete this.next;
+    delete this.prev;
+    return this;
+  }
 }
 
-Node.prototype.clearConnections = function () {
-  this.next = null;
-  this.prev = null;
-};
-
-function DLL() {
-  this.head = null;
-  this.tail = null;
-  this.length = 0;
-}
-
-DLL.prototype.push = function (value) {
-  const newNode = new Node(value);
-  if (this.length === 0) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    this.tail.next = newNode;
-    newNode.prev = this.tail;
-    this.tail = newNode;
-  }
-  this.length++;
-};
-
-DLL.prototype.unshift = function (value) {
-  if (this.length === 0) {
-    this.push(value);
-  } else {
-    const newNode = new Node(value);
-    this.head.prev = newNode;
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
-  }
-};
-
-DLL.prototype.pop = function () {
-  if (this.length === 0) return;
-  const poppedNode = this.tail;
-  if (this.length === 1) {
+class DoublyLinkedList{
+  constructor(){
     this.head = null;
     this.tail = null;
-  } else {
-    this.tail = poppedNode.prev;
-    poppedNode.prev = null;
-    this.tail.next = null;
+    this.length = 0;
   }
-  this.length--;
-  return poppedNode;
-};
 
-DLL.prototype.shift = function () {
-  if (this.length === 0) return;
-  if (this.length === 1) {
-    return this.pop();
-  } else {
-    const shiftedNode = this.head;
-    this.head = shiftedNode.next;
-    shiftedNode.next = null;
-    this.head.prev = null;
+  push(value){
+    const newNode = new Node(value);
+    if(this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    return ++this.length;
+  }
+
+  unshift(value){
+    if(this.length === 0) return this.push(value);
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    return ++this.length;
+  }
+
+  pop(){
+    if(this.length === 0) return;
+    const poppedNode = this.tail;
+    if(this.length === 1){
+      this.head = null;
+      this.tail = null;
+    }else{
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
     this.length--;
-    return shiftedNode;
+    return poppedNode.clearConnections();
   }
-};
 
-DLL.prototype.get = function (index) {
-  const middle = Math.floor(this.length / 2);
-  let current, counter;
-  if (index >= middle) {
-    counter = this.length - 1;
-    current = this.tail;
-    while (counter !== index) {
-      current = current.prev;
-      counter--;
+  shift(){
+    if(this.length === 0) return this.pop();
+    const shiftedNode = this.head;
+    if(this.length === 1){
+      this.head = null;
+      this.tail = null;
+    }else{
+      this.head = this.head.next;
+      this.head.prev = null;
     }
-  } else {
-    counter = 0;
-    current = this.head;
-    while (counter !== index) {
+    this.length--;
+    return shiftedNode.clearConnections();
+  }
+
+  get(index){
+    if(index < 0 || index > this.length-1) return;
+    const mid = Math.floor(this.length / 2);
+    let current;
+    let count;
+    if(index >= mid){
+      current = this.tail;
+      count = this.length - 1;
+      while(count !== index){
+        current = current.prev;
+        count--;
+      }
+    }else{
+      current = this.head;
+      count = 0;
+      while(count !== index){
+        current = current.next;
+        count++;
+      }
+    }
+    return current;
+  }
+
+  set(index , value){
+    const foundNode = this.get(index);
+    foundNode && (foundNode.value = value);
+    return foundNode;
+  }
+
+  insert(index , value){
+    if(index < 0 || index > this.length -1) return;
+    const newNode = new Node(value);
+    const prev = this.get(index-1);
+    const next = this.get(index);
+    prev.next = newNode;
+    newNode.prev = prev;
+    next.prev = newNode;
+    newNode.next = next;
+    return ++this.length;
+  }
+
+  remove(index){
+    if(index === 0 ) return this.shift();
+    if(index === this.length - 1) return this.pop();
+    const prev = this.get(index-1);
+    const current = this.get(index);
+    const next = this.get(index+1);
+    prev.next = next;
+    next.prev = prev;
+    return current.clearConnections();
+  }
+
+  traverse(){
+    let current = this.head;
+    while(current){
+      console.log(current.value);
       current = current.next;
-      counter++;
     }
   }
-  return current;
-};
+}
 
-DLL.prototype.set = function (index, value) {
-  const foundNode = this.get(index);
-  if (foundNode) foundNode.value = value;
-};
+const list = new DoublyLinkedList();
+list.push(0);
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
 
-DLL.prototype.remove = function (index) {
-  if (index === 0) return this.shift();
-  if (index === this.length - 1) return this.pop();
-  let current = this.get(index);
-  let prev = current.prev;
-  let next = current.next;
-  prev.next = next;
-  next.prev = prev;
-  current.clearConnections();
-  this.length--;
-  return current;
-};
+list.remove(0)
 
-DLL.prototype.insert = function (index, value) {
-  if (index === 0) return this.unshift(value);
-  if (index === this.length - 1) return this.push(value);
-  const newNode = new Node(value);
-  let current = this.get(index);
-  let prev = current.prev;
-  prev.next = newNode;
-  newNode.prev = prev;
-  newNode.next = current;
-  current.prev = newNode;
-};
-
-// const list = new DLL();
-// list.push(1);
-// list.push(2);
-// list.push(3);
-// list.push(4);
-// list.insert(2, 999999);
-
-// for (let i = 0; i < 4; i++) {
-//   console.log(list.shift().value);
-// }
+list.traverse();
