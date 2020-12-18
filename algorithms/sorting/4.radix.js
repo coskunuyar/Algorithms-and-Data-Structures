10d24538996ce1a82edf3fbb9f2ba919f6deb71f
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-18 22:13:33 +0300

    finalised

--- algorithms/sorting/4.radix.js
+++ algorithms/sorting/4.radix.js
@@ -1,31 +1,30 @@
-
-const getDigit = (num , index) => {
-  const result = Number(Math.abs(num).toString().split('').reverse()[index])
-  return result ? result : 0;
-}
-
-const getDigitCount = num => Math.abs(num).toString().length;
-
-const getMaxDigitCount = (arr) => {
-  let maxCount = 0;
-  for(let i=0; i<arr.length; i++){
-    maxCount = Math.max(maxCount , getDigitCount(arr[i]));
-  }
-  return maxCount;
-}
-
 const radixSort = (arr) => {
   const maxDigitCount = getMaxDigitCount(arr);
   for(let i=0; i<maxDigitCount; i++){
-    const buckets = [[],[],[],[],[],[],[],[],[],[]];
+    const digitBuckets = [[],[],[],[],[],[],[],[],[],[]];
     for(let j=0; j<arr.length; j++){
-      const digit = getDigit(arr[j] , i);
-      buckets[digit].push(arr[j]);
+      const digit = getDigit(arr[j], i);
+      digitBuckets[digit].push(arr[j]);
     }
     arr = [];
-    buckets.forEach(bucket => {
+    digitBuckets.forEach(bucket => {
       arr = [...arr , ...bucket];
     })
   }
   return arr;
 }
+
+const getMaxDigitCount = (arr) => {
+  let maxDigitCount = 0;
+  for(let i=0; i<arr.length; i++){
+    maxDigitCount = Math.max(maxDigitCount , Math.abs(arr[i]).toString().length)
+  }
+  return maxDigitCount;
+}
+
+const getDigit = (num , index ) => {
+  const result = Number(Math.abs(num).toString().split('').reverse()[index])
+  return result ? result : 0;
+}
+
+console.log(radixSort([5,4,3,2,1]));
