4461f0d3e619e25f0a03b19833f1d4d141d72c4a
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 13:20:41 +0300

    finalised

--- data-structures/5.BinaryHeap/index.js
+++ data-structures/5.BinaryHeap/index.js
@@ -1,75 +1,78 @@
-function BinaryMaxHeap() {
-  this.storage = [];
-}
-
-BinaryMaxHeap.prototype.push = function (value) {
-  this.storage.push(value);
-  this.bubbleUp();
-};
-
-BinaryMaxHeap.prototype.shift = function () {
-  const max = this.storage[0];
-  const last = this.storage.pop();
-
-  if (this.storage.length > 0) {
-    this.storage[0] = last;
-    this.sinkDown();
+class BinaryMaxHeap{
+  constructor(){
+    this.storage = [];
   }
 
-  return max;
-};
-
-BinaryMaxHeap.prototype.bubbleUp = function (value) {
-  let currentIndex = this.storage.length - 1;
-  let parentIndex = Math.floor((currentIndex - 1) / 2);
-  while (this.storage[currentIndex] > this.storage[parentIndex]) {
-    let temp = this.storage[currentIndex];
-    this.storage[currentIndex] = this.storage[parentIndex];
-    this.storage[parentIndex] = temp;
-
-    currentIndex = parentIndex;
-    parentIndex = Math.floor((currentIndex - 1) / 2);
+  push(value){
+    this.storage.push(value);
+    this.bubbleUp();
   }
-};
 
-BinaryMaxHeap.prototype.sinkDown = function () {
-  let currentIndex = 0;
-  while (true) {
-    let leftChildIndex = currentIndex * 2 + 1;
-    let rightChildIndex = currentIndex * 2 + 2;
-    let leftChild = this.storage[leftChildIndex];
-    let rightChild = this.storage[rightChildIndex];
-    let currentChild = this.storage[currentIndex];
-
-    let swapIndex;
-
-    if (leftChildIndex < this.storage.length && leftChild > currentChild) {
-      swapIndex = leftChildIndex;
+  shift(){
+    const max = this.storage[0];
+    const poppedNode = this.storage.pop();
+    if(this.storage.length > 0){
+      this.storage[0] = poppedNode;
+      this.sinkDown();
     }
+    return max;
+  }
 
-    if (rightChildIndex < this.storage.length && rightChild > currentIndex) {
-      if (!swapIndex) {
-        swapIndex = rightChildIndex;
-      } else if (swapIndex && rightChild > leftChild) {
-        swapIndex = rightChildIndex;
+  bubbleUp(){
+    let currentIndex = this.storage.length - 1;
+    let parentIndex = Math.floor(( currentIndex -1 ) / 2);
+    while(this.storage[currentIndex] > this.storage[parentIndex]){
+      const temp = this.storage[parentIndex];
+      this.storage[parentIndex] = this.storage[currentIndex];
+      this.storage[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(( currentIndex - 1) / 2);
+    }
+  }
+
+  sinkDown(){
+    let currentIndex = 0;
+    while(true){
+      let leftChildIndex = 2*currentIndex + 1;
+      let rightChildIndex = 2*currentIndex + 2;
+      let leftChild = this.storage[leftChildIndex];
+      let rightChild = this.storage[rightChildIndex];
+      let current = this.storage[currentIndex];
+
+      let swapIndex;
+      if(leftChildIndex < this.storage.length && leftChild > current){
+        swapIndex = leftChildIndex;
+      }
+
+      if(rightChildIndex < this.storage.length && rightChild > current){
+        if(!swapIndex){
+          swapIndex = rightChildIndex;
+        }else{
+          if(rightChild > current){
+            swapIndex = rightChildIndex;
+          }
+        }
+      }
+
+      if(!swapIndex) break;
+      if(swapIndex){
+        const temp = this.storage[swapIndex];
+        this.storage[swapIndex] = this.storage[currentIndex];
+        this.storage[currentIndex] = temp;
+        currentIndex = swapIndex;
       }
     }
-
-    if (!swapIndex) break;
-    let temp = this.storage[currentIndex];
-    this.storage[currentIndex] = this.storage[swapIndex];
-    this.storage[swapIndex] = temp;
-
-    currentIndex = swapIndex;
   }
-};
 
-// const binaryMaxHeap = new BinaryMaxHeap();
-// binaryMaxHeap.push(5);
-// binaryMaxHeap.push(20);
-// binaryMaxHeap.push(-3);
-// binaryMaxHeap.push(100);
+}
 
-// for (let i = 0; i < 4; i++) {
-//   console.log(binaryMaxHeap.shift());
-// }
+const binaryMaxHeap = new BinaryMaxHeap();
+binaryMaxHeap.push(5);
+binaryMaxHeap.push(20);
+binaryMaxHeap.push(-3);
+binaryMaxHeap.push(100);
+
+for (let i = 0; i < 4; i++) {
+  console.log(binaryMaxHeap.shift());
+}
