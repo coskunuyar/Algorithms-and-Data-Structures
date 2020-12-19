5c0913309c088aad8dff510c97046ed763f4a6e7
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 12:39:46 +0300

    finalised

--- data-structures/4.BinarySearchTree/index.js
+++ data-structures/4.BinarySearchTree/index.js
@@ -1,105 +1,103 @@
-function Node(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-function BST() {
-  this.root = null;
-}
+class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  }
 
-BST.prototype.insert = function (value) {
-  const newNode = new Node(value);
-  if (!this.root) {
-    this.root = newNode;
-  } else {
-    let current = this.root;
-    while (true) {
-      if (current.value > value) {
-        if (current.left) {
-          current = current.left;
-        } else {
-          current.left = newNode;
-          return;
+  insert(value){
+    const newNode = new Node(value);
+    if(!this.root){
+      this.root = newNode;
+    }else{
+      let current = this.root;
+      let isNotInserted = true;
+      while(isNotInserted){
+        if(value < current.value){
+          if(current.left){
+            current = current.left
+          }else{
+            current.left = newNode;
+            isNotInserted = false;
+          }
+        }else if(value > current.value){
+          if(current.right){
+            current = current.right;
+          }else{
+            current.right = newNode;
+            isNotInserted = false;
+          }
         }
-      } else if (current.value < value) {
-        if (current.right) {
-          current = current.right;
-        } else {
-          current.right = newNode;
-          return;
-        }
-      } else {
-        return;
       }
     }
   }
-};
 
-BST.prototype.BFS = function () {
-  if (!this.root) return;
-  const queue = [];
-  const visited = [];
-
-  queue.push(this.root);
-
-  while (queue.length) {
-    let shiftedNode = queue.shift();
-    visited.push(shiftedNode.value);
-
-    shiftedNode.left && queue.push(shiftedNode.left);
-    shiftedNode.right && queue.push(shiftedNode.right);
+  bfs(){
+    if(!this.root) return;
+    const visited = [];
+    const stack = [this.root];
+    while(stack.length){
+      const poppedNode = stack.shift();
+      visited.push(poppedNode.value);
+      poppedNode.left && stack.push(poppedNode.left);
+      poppedNode.right && stack.push(poppedNode.right);
+    }
+    return visited;
   }
 
-  return visited;
-};
+  dfsPreOrder(){
+    if(!this.root) return;
+    const visited = [];
+    const traverse = (node) => {
+      visited.push(node.value);
+      node.left && traverse(node.left);
+      node.right && traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
 
-BST.prototype.DFSPreOrder = function () {
-  if (!this.root) return;
-  const visited = [];
-  const traverse = (node) => {
-    visited.push(node.value);
-    node.left && traverse(node.left);
-    node.right && traverse(node.right);
-  };
-  traverse(this.root);
-  return visited;
-};
+  dfsInOrder(){
+    if(!this.root) return;
+    const visited = [];
+    const traverse = (node) => {
+      node.left && traverse(node.left);
+      visited.push(node.value);
+      node.right && traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
 
-BST.prototype.DFSInOrder = function () {
-  if (!this.root) return;
-  const visited = [];
-  const traverse = (node) => {
-    node.left && traverse(node.left);
-    visited.push(node.value);
-    node.right && traverse(node.right);
-  };
-  traverse(this.root);
-  return visited;
-};
+  dfsPostOrder(){
+    if(!this.root) return;
+    const visited = [];
+    const traverse = (node) => {
+      node.left && traverse(node.left);
+      node.right && traverse(node.right);
+      visited.push(node.value);
+    }
+    traverse(this.root);
+    return visited;
+  }
+}
 
-BST.prototype.DFSPostOrder = function () {
-  if (!this.root) return;
-  const visited = [];
-  const traverse = (node) => {
-    node.left && traverse(node.left);
-    node.right && traverse(node.right);
-    visited.push(node.value);
-  };
-  traverse(this.root);
-  return visited;
-};
+const tree = new BinarySearchTree();
+tree.insert(5);
+tree.insert(3);
+tree.insert(7);
+tree.insert(2);
+tree.insert(4);
+tree.insert(6);
+tree.insert(8);
 
-BST.prototype.contains = function (value) {
-  if (!this.root) return;
-  return this.BFS().contains(value);
-};
-
-const bst = new BST();
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
-bst.insert(-10);
-bst.insert(99);
-
-console.log(bst.DFSPostOrder());
+console.log(tree.bfs());
+console.log(tree.dfsPreOrder())
+console.log(tree.dfsInOrder())
+console.log(tree.dfsPostOrder())
