ec4cfd50ea335e937d9b36ebc1bbe4b77a48bcc8
Coskun Uyar <coskunuyarr@gmail.com>
2020-12-24 23:26:21 +0300

    search algorithms typescript migration

--- algorithms/searching/index.ts
+++ algorithms/searching/index.ts
@@ -1,8 +1,6 @@
-// Big O
-// Best: O(1)
+
 // Average: O(n)
-// Worst: O(n)
-const linearSearch = (arr , item) => {
+const linearSearch = (arr: number[] , item: number):number => {
   let index = -1;
   for(let i=0; i<arr.length; i++){
     if(arr[i] === item) index = i;
@@ -10,13 +8,10 @@ const linearSearch = (arr , item) => {
   return index;
 }
 
-// Big O
-// Best: O(1)
-// Average: O(log2n)
-// Worst: O(log2n)
-const binarySeach = (arr , item) => {
+// Average: O(logn)
+const binarySeach = (arr: number[] , item: number): number => {
   let start = 0;
-  let end = arr.length - 1;
+  let end = arr.length-1;
   let mid = Math.floor((start + end) / 2);
   while(arr[mid] !== item && start <= end){
     item < arr[mid] ? (end = mid-1) : (start = mid+1);
@@ -25,11 +20,8 @@ const binarySeach = (arr , item) => {
   return arr[mid] === item ? mid : -1;
 }
 
-// Big O
-// Best: O(1)
 // Average: O(n)
-// Worst: O(n)
-const naiveStringSearch = (string , key) => {
+const naiveStringSearch = (string: string , key: string): number => {
   let count = 0;
   for(let i=0; i<string.length; i++){
     for(let j=0; j<key.length; j++){
